20dcc9b3e9c62e1a8d5f7724bb3d0fca51a23c77
Michael Mooney <51172274+MichaelCMooney@users.noreply.github.com>
2021-11-08 09:33:19 -0500

    Update KeepMeActive.ps1

--- KeepMeActive.ps1
+++ KeepMeActive.ps1
@@ -1,3 +1,6 @@
+# Found that sending the "." key is imperfect. This solution requires another program to be open to place the "." key into.
+# This should instead be replaced by the "F15" key, which is uncommon on most keyboards so most programs don't accept it, but WinOS will.
+
 param($minutes = 60)
 
 $myshell = New-Object -com "Wscript.Shell"
